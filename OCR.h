94d8b7013ff80eb658564b63b3d9dda67d0fc2ac
Artur Silva <marturgsilva@gmail.com>
2017-01-12 21:26:49 -0200

    Initial commit

---
+++ OCR.h
@@ -0,0 +1,72 @@
+/*****************************************************************************
+*   Number Plate Recognition using SVM and Neural Networks
+******************************************************************************
+*   by David Millán Escrivá, 5th Dec 2012
+*   http://blog.damiles.com
+******************************************************************************
+*   Ch5 of the book "Mastering OpenCV with Practical Computer Vision Projects"
+*   Copyright Packt Publishing 2012.
+*   http://www.packtpub.com/cool-projects-with-opencv/book
+*****************************************************************************/
+
+#ifndef OCR_h
+#define OCR_h
+
+#include <string.h>
+#include <vector>
+
+#include "Plate.h"
+
+#include "opencv/cv.h"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv/cvaux.h"
+#include "opencv/ml.h"
+
+using namespace std;
+using namespace cv;
+using namespace cv::ml;
+
+#define HORIZONTAL    1
+#define VERTICAL    0
+
+class CharSegment{
+public:
+    CharSegment();
+    CharSegment(Mat i, Rect p);
+    Mat img;
+    Rect pos;
+};
+
+class OCR {
+public:
+    bool _DEBUG;
+    bool saveSegments;
+        string filename;
+        static const int numCharacters;
+        static const char strCharacters[];
+        OCR(string trainFile);
+        OCR();
+        string run(Plate *input);
+        int charSize;
+        Mat preprocessChar(Mat in);
+        int classify(Mat f);
+        void train(Mat trainData, Mat trainClasses, int nlayers);
+        int classifyKnn(Mat f);
+        void trainKnn(Mat trainSamples, Mat trainClasses, int k);
+        Mat features(Mat input, int size);
+
+    private:
+        bool trained;
+        vector<CharSegment> segment(Plate input);
+        Mat Preprocess(Mat in, int newSize);        
+        Mat getVisualHistogram(Mat *hist, int type);
+        void drawVisualFeatures(Mat character, Mat hhist, Mat vhist, Mat lowData);
+        Mat ProjectedHistogram(Mat img, int t);
+        bool verifySizes(Mat r);
+        cv::Ptr<ANN_MLP> ann;
+        cv::Ptr<KNearest> knnClassifier;
+        int K;
+};
+
+#endif
